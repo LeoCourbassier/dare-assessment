b80333742770bd32c54216b0c3b412eff2ac4169
Leonardo Courbassier Martins <leonardo.courbassier@sumup.com>
2021-06-08 01:27:17 -0300

    feat: first controller (health check)

---
+++ src/controllers/healthCheck.js
@@ -0,0 +1,9 @@
+import HealthCheckView from '../views/responses/healthCheck';
+
+export default class HealthCheckController {
+  static get(_request, response) {
+    response
+      .code(200)
+      .send(HealthCheckView());
+  }
+}
---
+++ src/views/responses/healthCheck.js
@@ -0,0 +1,6 @@
+const HealthCheckView = () => ({
+  code: 200,
+  message: 'I\'m alive',
+});
+
+export default HealthCheckView;
