339aa8cc7d240539ede9243464c5b692a2f70202
Leonardo Courbassier Martins <leonardo.courbassier@sumup.com>
2021-06-09 00:26:14 -0300

    chore: add server class
    This will help in the tests when we need to stop the server

--- src/app.js
+++ src/app.js
@@ -5,33 +5,48 @@ import authenticationMiddleware from './middlewares/authentication';
 import dareMiddleware from './middlewares/dare';
 import setupRoutes from './routes/index';
 
-// Get jwt secret from env var, fallbacks to 's3cr3t'
-const JWT_SECRET = process.env.JWT_SECRET || 's3cr3t';
+export default class Server {
+  static getInstance() {
+    if (!this.instance) {
+      this.instance = new Server();
 
-// Get port to run the server, fallbacks to 3000
-const PORT = process.env.PORT || 3000;
+      // Get port to run the server, fallbacks to 3000
+      this.instance.PORT = process.env.PORT || 3000;
 
-// Instantiate fastify
-const Fastify = fastify({ logger: true });
+      // Get jwt secret from env var, fallbacks to 's3cr3t'
+      this.instance.JWT_SECRET = process.env.JWT_SECRET || 's3cr3t';
 
-// Register JWT utils
-Fastify.register(fastifyJWT, {
-  secret: JWT_SECRET,
-});
+      // Instantiate fastify
+      this.instance.fastifyInstance = fastify({ logger: true });
 
-// Register Auth package
-Fastify.register(fastifyAuth);
+      // Register JWT utils
+      this.instance.fastifyInstance.register(fastifyJWT, {
+        secret: this.instance.JWT_SECRET,
+      });
 
-// Register middlewares
-Fastify.decorate('authMiddleware', authenticationMiddleware);
-Fastify.decorate('dareMiddleware', dareMiddleware);
+      // Register Auth package
+      this.instance.fastifyInstance.register(fastifyAuth);
 
-// Register our routes
-Fastify.register(setupRoutes, { prefix: '/api/v1' });
+      // Register middlewares
+      this.instance.fastifyInstance.decorate('authMiddleware', authenticationMiddleware);
+      this.instance.fastifyInstance.decorate('dareMiddleware', dareMiddleware);
 
-// Init webserver
-Fastify.listen(PORT, '0.0.0.0')
-  .catch((err) => {
-    Fastify.log.error(`starting server: ${err}`);
-    process.exit(1);
-  });
+      // Register our routes
+      this.instance.fastifyInstance.register(setupRoutes, { prefix: '/api/v1' });
+    }
+
+    return this.instance;
+  }
+
+  listen() {
+    this.fastifyInstance.listen(this.PORT, '0.0.0.0')
+      .catch((err) => {
+        this.fastifyInstance.log.error(`starting server: ${err}`);
+        process.exit(1);
+      });
+  }
+
+  async stop() {
+    await this.fastifyInstance.close();
+  }
+}
